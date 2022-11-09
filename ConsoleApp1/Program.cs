0c2b6a8049c822a2973d6896825c0ea43eeaa99e
hatobanono <chris00iq@gmail.com>
2022-11-09 10:42:46 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> items = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			Func<int, bool> isEven = n => n % 2 == 0;
+
+			List<int> EvenItems = GetEvenItems(items, isEven);
+		}
+
+		static List<int> GetEvenItems(List<int> source, Func<int, bool> func)
+		{
+			var result = new List<int>();
+
+			foreach(var item in source)
+			{
+				if(func(item) == true)
+				{
+					result.Add(item);
+				}
+			}
+			return result;
+		}
+	} 
+}
